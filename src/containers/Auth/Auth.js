48c631c674476aebc5cbdfc49d2df52f2aa22b9a
i_am_yevhen <yevheniiperekopskyi@gmail.com>
2019-10-27 17:22:38 +0300

    закончил приложение

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,9 +3,12 @@ import './Auth.css';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import is from 'is_js';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { auth } from '../../store/actions/auth';
 
-export default class Auth extends Component {
+
+
+class Auth extends Component {
 
   state = {
     isFormValid: false,
@@ -37,32 +40,22 @@ export default class Auth extends Component {
     }
   }
 
-  loginHandler = async () => {
-    const authData = {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true
-    }
-    try {
-      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAzzS1E5llL73H1kqH5r-nnPI-HHpi5d2M', authData)
-      console.log(response.data)
-    } catch (e) {
-      console.log(e)
-    }
+  loginHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      true
+    )
   }
-  registerHandler = async () => {
-    const authData = {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true
-    }
-    try {
-      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAzzS1E5llL73H1kqH5r-nnPI-HHpi5d2M', authData)
-      console.log(response.data)      
-    } catch (e) {
-      console.log(e)       
-    }
+
+  registerHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      false
+    )
   }
+
   submitHandler = event => {
     event.preventDefault()
   }
@@ -158,3 +151,11 @@ export default class Auth extends Component {
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Auth)
--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -5,7 +5,8 @@ import Input from '../../components/UI/Input/Input';
 import Select from '../../components/UI/Select/Select';
 import { createControl, validate, validateForm } from '../../form/formFramework';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
-import axios from '../../axios/axios-quiz';
+import { connect } from 'react-redux';
+import {createQuizQuestion, finishCreateQuiz} from "../../store/actions/create";
 
 
 function createOptionControl(number) {
@@ -29,30 +30,26 @@ function createFormControls() {
   }
 }
 
-export default class QuizCreator extends Component {
+class QuizCreator extends Component {
 
   state = {
-    quiz: [],
     isFormValid: false,
     rightAnswerId: 1,
     formControls: createFormControls()
   }
 
-  submitHandler = (e) => {
+  submitHandler = e => {
     e.preventDefault()
   }
 
   addQuestionHandler = (event) => {
     event.preventDefault()
 
-    const quiz = this.state.quiz.concat()
-    const index = quiz.length + 1
-
     const {question, option1, option2, option3, option4} = this.state.formControls
 
     const questionItem = {
       question: question.value,
-      id: index,
+      id: this.props.quiz.length + 1,
       rightAnswerId: this.state.rightAnswerId,
       answers: [
         { text: option1.value, id: option1.id },
@@ -62,31 +59,24 @@ export default class QuizCreator extends Component {
       ]
     }
 
-    quiz.push(questionItem)
+    this.props.createQuizQuestion(questionItem)
 
     this.setState({
-      quiz,
       isFormValid: false,
       rightAnswerId: 1,
       formControls: createFormControls()
     })
   }
 
-  createQuizHandler = async event => {
+  createQuizHandler = event => {
     event.preventDefault()
 
-    try {
-      await axios.post('/quizes.json', this.state.quiz)
-
-      this.setState({
-        quiz: [],
-        isFormValid: false,
-        rightAnswerId: 1,
-        formControls: createFormControls()
-      })
-    } catch (e) {
-      console.log(e)  
-    }
+    this.setState({
+      isFormValid: false,
+      rightAnswerId: 1,
+      formControls: createFormControls()
+    })
+    this.props.finishCreateQuiz()
   }
 
   changeHandler = (value, controlName) => {
@@ -112,7 +102,7 @@ export default class QuizCreator extends Component {
       return (
         <Auxiliary>
           <Input
-            key = {index}
+            key={index}
             label={control.label}
             valid={control.valid}
             value={control.value}
@@ -128,7 +118,6 @@ export default class QuizCreator extends Component {
   }
 
   selectChangeHanler = event => {
-    console.log(event.target.value);
     this.setState({
       rightAnswerId: +event.target.value
     })
@@ -167,7 +156,7 @@ export default class QuizCreator extends Component {
             <Button
               type="success"
               onClick={this.createQuizHandler}
-               disabled={this.state.quiz.length === 0}
+               disabled={this.props.quiz.length === 0}
             >
               Создать тест
             </Button>
@@ -177,3 +166,18 @@ export default class QuizCreator extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    quiz: state.create.quiz
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    createQuizQuestion: item => dispatch(createQuizQuestion(item)),
+    finishCreateQuiz: () => dispatch(finishCreateQuiz())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizCreator)
